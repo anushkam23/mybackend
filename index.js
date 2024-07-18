0b461f40561766af4093b4998460f7cd08a0ad5c
Anushka <anushkam254@gmail.com>
2024-07-18 22:18:14 +0530

    Added

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubData={
+  "login": "anushkam23",
+  "id": 140802626,
+  "node_id": "U_kgDOCGR6Qg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/140802626?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/anushkam23",
+  "html_url": "https://github.com/anushkam23",
+  "followers_url": "https://api.github.com/users/anushkam23/followers",
+  "following_url": "https://api.github.com/users/anushkam23/following{/other_user}",
+  "gists_url": "https://api.github.com/users/anushkam23/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/anushkam23/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/anushkam23/subscriptions",
+  "organizations_url": "https://api.github.com/users/anushkam23/orgs",
+  "repos_url": "https://api.github.com/users/anushkam23/repos",
+  "events_url": "https://api.github.com/users/anushkam23/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/anushkam23/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 17,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2023-07-28T09:25:15Z",
+  "updated_at": "2024-07-16T22:36:43Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +52,11 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
   res.send("<h2>Chai aur code</h2>")
+})
+
+//callback to send github data which is in json format
+app.get('/github',(req,res)=>{
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
